699e30d6d04d81df02125a90221d1556bb65089f
HarwordLiu <harwordliu@Gmail.com>
2017-08-12 18:47:13 +0800

    init

---
+++ Ing/Ing/Manager/HLCoreDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  HLCoreDataManager.swift
+//  Ing
+//
+//  Created by 刘浩 on 2017/8/12.
+//  Copyright © 2017年 刘浩. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import CloudKit
+
+typealias networkOperationResult = (_ success: Bool, _ errorMessage: String?) -> Void
+
+class HLCoreDataManager: NSObject {
+    
+    
+}
